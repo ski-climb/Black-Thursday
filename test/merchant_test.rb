74fd64873730e8092ed241f3a7fe50d04bf2568d
Alex Fosco <alex.hf88@gmail.com>
2016-10-31 16:33:02 -0600

    merchants have names and ids

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,31 +4,18 @@ require './lib/merchant'
 
 class MerchantTest < Minitest::Test
 
-  def test_it_exists
-    assert Merchant.new
-  end
-
   def test_merchants_have_ids
     merchant_id = 12345678
-    m = Merchant.new(id: merchant_id)
+    merchant_name = "IronCompassFlight"
+    m = Merchant.new(merchant_id, merchant_name)
     assert_equal merchant_id, m.id
   end
 
-  def test_default_id_for_merchants_is_000_000_00
-    merchant_id = nil
-    m = Merchant.new(id: merchant_id)
-    assert_equal 000_000_00, m.id
-  end
-
   def test_merchants_have_names
+    merchant_id = 12345678
     merchant_name = "IronCompassFlight"
-    m = Merchant.new(name: merchant_name)
+    m = Merchant.new(merchant_id, merchant_name)
     assert_equal merchant_name, m.name
   end
 
-  def test_merchants_have_default_name
-    merchant_name = nil
-    m = Merchant.new(name: merchant_name)
-    assert_equal "No Merchant Name", m.name
-  end
 end
