c0bc7ffb7c40c6f276a252efa833e8c868ed8181
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-10-31 22:02:05 -0600

    Be more descriptive with Merchant Repository test names

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -28,3 +28,8 @@ class MerchantRepository
   end
 
 end
+
+
+
+
+
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -6,13 +6,13 @@ require './lib/merchant'
 class MerchantRepositoryTest < Minitest::Test
 
   def setup
-    @mr = MerchantRepository.new
+    @merchant_repository = MerchantRepository.new
     @merchant_1 = Merchant.new(1, "King Soopers")
     @merchant_2 = Merchant.new(2, "Whole Foods")
     @merchant_3 = Merchant.new(3, "Subway")
-    @mr << @merchant_1
-    @mr << @merchant_2
-    @mr << @merchant_3
+    @merchant_repository << @merchant_1
+    @merchant_repository << @merchant_2
+    @merchant_repository << @merchant_3
   end
 
   def test_it_exists
@@ -20,43 +20,43 @@ class MerchantRepositoryTest < Minitest::Test
   end
 
   def test_it_has_no_merchants_when_initialized
-    mr = MerchantRepository.new
-    assert_equal [], mr.all
+    merchant_repository = MerchantRepository.new
+    assert_equal [], merchant_repository.all
   end
 
   def test_merchants_can_be_added_to_the_repository
-    assert_equal 3, @mr.all.count
-    @mr.all.include?(@merchant_1)
-    @mr.all.include?(@merchant_2)
-    @mr.all.include?(@merchant_3)
+    assert_equal 3, @merchant_repository.all.count
+    @merchant_repository.all.include?(@merchant_1)
+    @merchant_repository.all.include?(@merchant_2)
+    @merchant_repository.all.include?(@merchant_3)
   end
 
   def test_merchants_can_be_found_by_id
-    assert_equal @mr.find_by_id(2), @merchant_2
+    assert_equal @merchant_repository.find_by_id(2), @merchant_2
   end
 
   def test_find_by_id_returns_nil_when_no_merchants_have_given_id
-    assert_nil @mr.find_by_id(12)
+    assert_nil @merchant_repository.find_by_id(12)
   end
 
   def test_find_by_name_returns_merchant_with_given_name
-    assert_equal @mr.find_by_name("Whole Foods"), @merchant_2
+    assert_equal @merchant_repository.find_by_name("Whole Foods"), @merchant_2
   end
 
   def test_find_by_name_is_case_insensitive
-    assert_equal @mr.find_by_name("whole foods"), @merchant_2
+    assert_equal @merchant_repository.find_by_name("whole foods"), @merchant_2
   end
 
   def test_find_by_name_returns_nil_when_no_merchants_match_given_name
-    assert_nil @mr.find_by_name("Pizza Hut")
+    assert_nil @merchant_repository.find_by_name("Pizza Hut")
   end
 
   def test_find_all_by_name_returns_merchant_with_given_name_stub
     merchant_4 = Merchant.new(4, "Dominoe's Pizza")
     merchant_5 = Merchant.new(5, "Pizza Hut")
-    @mr.all << merchant_4
-    @mr.all << merchant_5
-    results = @mr.find_all_by_name("zza")
+    @merchant_repository.all << merchant_4
+    @merchant_repository.all << merchant_5
+    results = @merchant_repository.find_all_by_name("zza")
     assert results.include?(merchant_4)
     assert results.include?(merchant_5)
   end
@@ -64,18 +64,18 @@ class MerchantRepositoryTest < Minitest::Test
   def test_find_all_by_name_is_case_insensitive
     merchant_4 = Merchant.new(4, "Dominoe's Pizza")
     merchant_5 = Merchant.new(5, "Pizza Hut")
-    @mr.all << merchant_4
-    @mr.all << merchant_5
-    results = @mr.find_all_by_name("PIZZA")
+    @merchant_repository.all << merchant_4
+    @merchant_repository.all << merchant_5
+    results = @merchant_repository.find_all_by_name("PIZZA")
     assert results.include?(merchant_4)
     assert results.include?(merchant_5)
   end
 
   def test_find_all_by_name_returns_nil_when_no_merchants_match_given_name_stub
-    assert_equal [], @mr.find_all_by_name("Pizza")
+    assert_equal [], @merchant_repository.find_all_by_name("Pizza")
   end
 
   def test_find_all_by_name_can_return_one_match
-    assert_equal [@merchant_2], @mr.find_all_by_name("Whole Foods")
+    assert_equal [@merchant_2], @merchant_repository.find_all_by_name("Whole Foods")
   end
 end
