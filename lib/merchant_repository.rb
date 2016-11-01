7277fe78be566ff0e2fbf279de788f09dc9bba95
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-10-31 21:27:22 -0600

    WIP most Merchant Repository tests passing

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,30 @@
+class MerchantRepository
+  attr_reader :all
+
+  def initialize
+    @all = []
+  end
+
+  def <<(merchant)
+    all.push(merchant)
+  end
+
+  def find_by_id(id)
+    all.find do |merchant|
+      merchant.id == id
+    end
+  end
+
+  def find_by_name(name)
+    all.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+  end
+
+  def find_all_by_name(name_stub)
+    all.find_all do |merchant|
+      merchant.name.downcase.include?(name_stub.downcase)
+    end
+  end
+
+end
--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -5,92 +5,77 @@ require './lib/merchant'
 
 class MerchantRepositoryTest < Minitest::Test
 
+  def setup
+    @mr = MerchantRepository.new
+    @merchant_1 = Merchant.new(1, "King Soopers")
+    @merchant_2 = Merchant.new(2, "Whole Foods")
+    @merchant_3 = Merchant.new(3, "Subway")
+    @mr << @merchant_1
+    @mr << @merchant_2
+    @mr << @merchant_3
+  end
+
   def test_it_exists
     assert MerchantRepository.new
   end
 
   def test_it_has_no_merchants_when_initialized
+    mr = MerchantRepository.new
     assert_equal [], mr.all
   end
 
-  def test_it_has_merchants
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_same_elements mr.all, [merchant_1, merchant_2, merchant_3]
+  def test_merchants_can_be_added_to_the_repository
+    assert_equal 3, @mr.all.count
+    @mr.all.include?(@merchant_1)
+    @mr.all.include?(@merchant_2)
+    @mr.all.include?(@merchant_3)
   end
 
   def test_merchants_can_be_found_by_id
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_equal mr.find_by_id(2), merchant_2
+    assert_equal @mr.find_by_id(2), @merchant_2
   end
 
   def test_find_by_id_returns_nil_when_no_merchants_have_given_id
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_nil mr.find_by_id(12)
+    assert_nil @mr.find_by_id(12)
   end
 
   def test_find_by_name_returns_merchant_with_given_name
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_equal mr.find_by_name("Whole Foods"), merchant_2
+    assert_equal @mr.find_by_name("Whole Foods"), @merchant_2
+  end
+
+  def test_find_by_name_is_case_insensitive
+    assert_equal @mr.find_by_name("whole foods"), @merchant_2
   end
 
   def test_find_by_name_returns_nil_when_no_merchants_match_given_name
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_nil mr.find_by_name("Pizza Hut")
+    assert_nil @mr.find_by_name("Pizza Hut")
   end
 
   def test_find_all_by_name_returns_merchant_with_given_name_stub
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
     merchant_4 = Merchant.new(4, "Dominoe's Pizza")
     merchant_5 = Merchant.new(5, "Pizza Hut")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    mr.all << merchant_4
-    mr.all << merchant_5
-    assert_same_elements mr.find_all_by_name("Pizza"), [merchant_4, merchant_5]
+    @mr.all << merchant_4
+    @mr.all << merchant_5
+    results = @mr.find_all_by_name("zza")
+    assert results.include?(merchant_4)
+    assert results.include?(merchant_5)
+  end
+
+  def test_find_all_by_name_is_case_insensitive
+    merchant_4 = Merchant.new(4, "Dominoe's Pizza")
+    merchant_5 = Merchant.new(5, "Pizza Hut")
+    @mr.all << merchant_4
+    @mr.all << merchant_5
+    results = @mr.find_all_by_name("PIZZA")
+    assert results.include?(merchant_4)
+    assert results.include?(merchant_5)
   end
 
   def test_find_all_by_name_returns_nil_when_no_merchants_match_given_name_stub
-    mr = MerchantRepository.new
-    merchant_1 = Merchant.new(1, "King Soopers")
-    merchant_2 = Merchant.new(2, "Whole Foods")
-    merchant_3 = Merchant.new(3, "Subway")
-    mr.all << merchant_1
-    mr.all << merchant_2
-    mr.all << merchant_3
-    assert_nil mr.find_all_by_name("Pizza")
+    assert_equal [], @mr.find_all_by_name("Pizza")
+  end
+
+  def test_find_all_by_name_can_return_one_match
+    assert_equal [@merchant_2], @mr.find_all_by_name("Whole Foods")
   end
 end
