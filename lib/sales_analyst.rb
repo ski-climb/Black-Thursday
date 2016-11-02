73b8ed6be4c205d2d8d3ec70fcceb080b3c6c848
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-11-02 17:00:50 -0600

    Calculate average items per merchant std dev

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -6,15 +6,28 @@ class SalesAnalyst
   end
 
   def average_items_per_merchant
-    result = number_of_items / number_of_merchants.to_f
+    result = total_number_of_items / total_number_of_merchants.to_f
     result.round(2)
   end
 
-  def number_of_items
+  def average_items_per_merchant_standard_deviation
+    numerator = all_merchants.map do |merchant|
+      (merchant.items.length - average_items_per_merchant)**2
+    end.reduce(:+)
+    denominator = total_number_of_merchants - 1
+    result = Math::sqrt(numerator / denominator)
+    return result.round(2)
+  end
+
+  def total_number_of_items
     sales_engine.items.all.length
   end
 
-  def number_of_merchants
+  def total_number_of_merchants
     sales_engine.merchants.all.length
+  end
+
+  def all_merchants
+    sales_engine.merchants.all
   end
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -29,4 +29,22 @@ class SalesAnalystTest < Minitest::Test
     assert_equal 2.88, analyst.average_items_per_merchant
   end
 
+  def test_it_calculates_average_items_per_merchant_standard_deviation
+    sales_engine = SalesEngine.from_csv({
+      :items =>     './test/fixtures/item_fixture.csv',
+      :merchants => './test/fixtures/merchant_fixture.csv'
+    })
+    analyst = SalesAnalyst.new(sales_engine)
+    assert_equal 1.08, analyst.average_items_per_merchant_standard_deviation
+  end
+
+  def test_it_calculates_average_items_per_merchant_standard_deviation_using_provided_data
+    skip "for speed!"
+    sales_engine = SalesEngine.from_csv({
+      :items =>     './data/items.csv',
+      :merchants => './data/merchants.csv'
+    })
+    analyst = SalesAnalyst.new(sales_engine)
+    assert_equal 3.26, analyst.average_items_per_merchant_standard_deviation
+  end
 end
